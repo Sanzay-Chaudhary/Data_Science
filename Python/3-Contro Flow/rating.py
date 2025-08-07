4000198c02cc31a66d04fe66290639047a57b214
Sanzay-Chaudhary <kctsanzu@gmail.com>
2025-08-07 11:58:28 +0545

    if/elif/els

---
+++ Python/3-Contro Flow/rating.py
@@ -0,0 +1,15 @@
+rating = 3
+if rating > 4.5:
+  print('Extraordinary')
+elif rating > 4:
+  print('Excellent')
+
+elif rating>3:
+  print('Good')
+
+elif rating>2 :
+  print('Fair')
+
+else:
+  print('Poor')   
+# This code evaluates a rating and prints a corresponding message based on the rating value.
