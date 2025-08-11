9ced9c8783b747a1d7ec6de8517e98ec6a3b8ab4
Sanzay-Chaudhary <kctsanzu@gmail.com>
2025-08-11 11:09:19 +0545

    numpy functions

---
+++ Numpy/Numpy Functions/functions.py
@@ -0,0 +1,10 @@
+import numpy as np
+temperatures = np.array([[50, 51, 54, 59, 59, 53, 54, 51],
+                         [45, 50, 38, 44, 40, 46, 43, 39]])
+
+print(np.min(temperatures))       # Output: 38
+print(np.max(temperatures))       # Output: 59
+print(np.sum(temperatures))       # Output: 776
+print(np.average(temperatures))   # Output: 48.5
+print(np.min(temperatures, axis=0))  # Minimum per column
+print(np.sum(temperatures, axis=1))  # Sum per row
