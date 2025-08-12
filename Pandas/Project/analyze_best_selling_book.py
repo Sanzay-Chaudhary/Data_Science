ac2ecb328cf2cfa23bcd2817985d93c8607d307a
Sanzay-Chaudhary <kctsanzu@gmail.com>
2025-08-12 17:42:11 +0545

    analyzed best selling book

--- Pandas/Project/analyze_best_selling_book.py
+++ Pandas/Project/analyze_best_selling_book.py
@@ -4,3 +4,27 @@ print(df.head())
 print(df.shape)
 print(df.columns)
 print(df.describe())
+
+# Data Cleaning
+df.drop_duplicates(inplace=True)
+
+# Renaming Columns for Clarity
+df.rename(columns={"Name": "Title", "Year": "Publication Year", "User Rating": "Rating"}, inplace=True)
+
+# Converting Data Types
+df["Price"] = df["Price"].astype(float)
+
+# Analyzing Author Popularity
+author_counts= df["Author"].value_counts()
+print(author_counts)
+
+# Average Rating by Genre
+avg_rating_by_genre = df.groupby("Genre")["Rating"].mean()
+print(avg_rating_by_genre)
+
+# Exporting the results
+# Exporting top selling authors to a CSV file
+author_counts.head(10).to_csv("top_authors.csv")
+
+# Export average rating by genre to a CSV file
+avg_rating_by_genre.to_csv("avg_rating_by_genre.csv")
