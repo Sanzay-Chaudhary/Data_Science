620e82fb8376dbe4851f88641e4b6ea29c51f0af
Sanzay-Chaudhary <kctsanzu@gmail.com>
2025-08-18 13:21:29 +0545

    constants

---
+++ Numpy/Project/main.py
@@ -0,0 +1,9 @@
+import numpy as np
+newarr = np.array([-3.1666, 3.667])
+print(newarr)
+
+newarr1 =np.trunc(newarr)
+print(newarr1)
+
+newarr2 = np.fix(newarr)
+print(newarr2)
---
+++ Scipy/1-Constants/constants.py
@@ -0,0 +1,43 @@
+from scipy import constants
+
+#Return the specified unit in meters(metric prefix)
+print(constants.centi)   
+
+#Return the specified unit in kilograms(mass)
+print(constants.gram)      
+
+#Return the specified unit in bytes(binary prefix)
+print(constants.kibi)    
+
+#Return the specified unit in radians(angle)
+print(constants.degree) 
+
+#Return the specified unit in seconds(time)
+print(constants.day)
+
+#Return the specified unit in meters(length)
+print(constants.mile)
+
+#Return the specified unit in pascals(pressure)
+print(constants.mmHg, "pascals")  
+
+#Return the specified unit in square meters(area)
+print(constants.acre)
+
+#Return the specified unit in cubic meters(volume)
+print(constants.liter) 
+
+#Return the specified unit in meters per second(speed)
+print(constants.kmh)
+
+#Return the specified unit in kelvins(temperature)
+print(constants.zero_Celsius) 
+
+#Return the specified unit in joules(energy)
+print(constants.calorie) 
+
+#Return the specified unit in watt(power)
+print(constants.hp)
+
+#Return the specified unit in newtons(force)
+print(constants.dyn)
